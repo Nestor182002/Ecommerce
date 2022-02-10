752f0f8c9dff820726b4c0383f3a65ec50e5faeb
Nestor Urueta <nestorurueta107@gmail.com>
2022-02-10 16:50:14 -0500

    categorys

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -3,20 +3,31 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Products(models.Model):
+class InheritedData(models.Model):
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(InheritedData):
+    name=models.CharField(max_length=70)
+    active=models.BooleanField(default=True)
+
+class Products(InheritedData):
     name=models.CharField(max_length=100)
     description=models.TextField()
     img=models.ImageField(null=True,blank=True)
     price=models.PositiveIntegerField()
     quantity=models.PositiveIntegerField(default=1)
     active=models.BooleanField(default=True)
+    categorys=models.ManyToManyField(Category)
 
     def __str__(self):
         return self.name
 
-
-
-class ProductsViewRecentList(models.Model):
+class ProductsViewRecentList(InheritedData):
     recent_user=models.ForeignKey(User,related_name='products_recent', on_delete=models.CASCADE,null=True,blank=True)
     recent_product=models.ForeignKey(Products,related_name='products_recent', on_delete=models.CASCADE)
     active=models.BooleanField(default=True)
