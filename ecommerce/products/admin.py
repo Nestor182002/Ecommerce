54bcda54b51450ab69474588f5d0124103b860b7
Nestor Urueta <nestorurueta107@gmail.com>
2022-02-16 14:04:07 -0500

    filtros de asc and des

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from products.models import Products, ProductsViewRecentList,Category
 
 # Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    list_display= ('name','price','quantity','created','active')
+    list_filter = ('price',)
+    search_fields = ['name', 'price',]
+    
+
 admin.site.register(Category)
-admin.site.register(Products)
+admin.site.register(Products,ProductsAdmin)
 admin.site.register(ProductsViewRecentList)
 
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,17 +1,19 @@
-from tempfile import template
 from django.core.paginator import Paginator
 from django.http import Http404
+# views
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+# dates
+import datetime
+from django.utils import timezone
+# models
 from products.models import ProductsViewRecentList
-
 from products.models import Products
 
 # Create your views here.
 
 class ProductsView(View):
-
+    '''search and pagination'''
     def get(self, request, *args, **kwargs):
         search = request.GET.get('search',None)
         if search != None:
@@ -19,16 +21,15 @@ class ProductsView(View):
         else:
             producs_list = Products.objects.all()
 
-        paginator_list = Paginator(producs_list, 15) # Show 25 contacts per page.
-
+        paginator_list = Paginator(producs_list, 15) # Show 15 products per page.
         page_number = request.GET.get('page')
         products_page = paginator_list.get_page(page_number)
         return  render(request, 'products/productslist.html', {'products_page':products_page})
 
 def DetailProducts(request,pk):
+    '''add to models recent products'''
     try:
         Product_Detail = Products.objects.get(id=pk)
-
         if request.user.is_authenticated:
             users=request.user
             if ProductsViewRecentList.objects.filter(recent_user=users,recent_product=Product_Detail,active=True).exists():
@@ -43,20 +44,43 @@ def DetailProducts(request,pk):
                 recent_user=users,
                 recent_product=Product_Detail,
                 )
-
+                
     except Products.DoesNotExist:
         raise Http404("Products does not exist")
     return render(request, 'products/detailproducts.html', {'object': Product_Detail})
 
-class RecentViewProduct(View):
 
+class RecentViewProduct(View):
     def get(self, request, *args, **kwargs):
         user=request.user
         recent=ProductsViewRecentList.objects.filter(recent_user=user,active=True)
         return render(request,'products/recentproducts.html', {'recents': recent})
 
+class ListCategoryProducts(View):
+    '''filter of products category'''
+    def get(self, request, *args, **kwargs):
+        data_cate=request.GET.get('cate',None)
+        ordering=request.GET.get('ordering',None)
+        #ordering  
+        if ordering == 'Asc':
+            ordering='id'
+        elif ordering == 'Des':
+            ordering='-id'
+        else:
+            ordering("id")
 
-class ListCategoryProducts(ListView):
-    model = Products
-    template_name='category/list_category.html'
-    fields = ['pk',]
+        # obtain day and week
+        today = datetime.date.today()
+        start_week = today - datetime.timedelta(today.weekday())
+        end_week = start_week + datetime.timedelta(7)
+        # t=today,s=week,v=more views
+        products_filter=Products.objects.all()
+        if data_cate != (None or ""):
+            if data_cate == 't':
+                products_filter=Products.objects.filter(created__day=today.day,created__month=today.month).order_by(ordering)
+            elif data_cate == 's':
+                products_filter=Products.objects.filter(created__gte=start_week,created__lte=end_week).order_by(ordering)
+        else:
+            products_filter=Products.objects.all().all().order_by(ordering)
+
+        return render(request,'category/list_category.html', {'products_list': products_filter})
